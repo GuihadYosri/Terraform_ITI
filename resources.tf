613d3ea35cd6754286f0899db299893be1cfda2e
Guihad Yosri <88772336+GuihadYosri@users.noreply.github.com>
2024-05-17 04:31:58 +0300

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -65,6 +65,12 @@ resource "aws_security_group" "app_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] 
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
 
   tags = {
     Name = "AppSG"
