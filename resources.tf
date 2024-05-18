c0423f2b56d1a4cc3bdce853676edace6dc0c10a
Guihad Yosri <88772336+GuihadYosri@users.noreply.github.com>
2024-05-18 23:43:46 +0300

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -173,7 +173,7 @@ resource "aws_security_group" "elasticache_sg" {
     from_port   = 6379  # Default port for Redis
     to_port     = 6379
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]  
+    cidr_blocks = ["0.0.0.0/16"]  
   }
 
   egress {
